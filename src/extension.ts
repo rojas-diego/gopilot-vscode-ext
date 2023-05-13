c1caf39bdf618e447d17fac5cc39762a7da480bd
Diego ROJAS <rojasdiegopro@gmail.com>
2023-05-13 10:49:39 +0800

    Use inline completion provider API, can activate, deactivate and accept

--- src/extension.ts
+++ src/extension.ts
@@ -2,72 +2,84 @@ import * as vscode from 'vscode';
 import axios from 'axios';
 
 export function activate(context: vscode.ExtensionContext) {
-	let coloredTextDecoration: vscode.TextEditorDecorationType | undefined;
-	let tokens: string | undefined;
+	let timeout: NodeJS.Timeout | undefined;
+	let activated = false;
 
-    const removeSuggestion = (editor: vscode.TextEditor) => {
-        if (coloredTextDecoration) {
-            editor.setDecorations(coloredTextDecoration, []);
-            coloredTextDecoration.dispose();
-            coloredTextDecoration = undefined;
-        }
-    };
+	const activateExtensionCommand = vscode.commands.registerCommand('gopilot-vscode-ext.activateExtension', () => {
+		vscode.window.showInformationMessage('Extension activated!');
+		activated = true;
+	});
 
-	let autoCompleteCommand = vscode.commands.registerCommand('gopilot-vscode-ext.autoComplete', async () => {
-        const editor = vscode.window.activeTextEditor;
-        if (!editor) {
-            vscode.window.showErrorMessage('No active editor found.');
-            return;
-        }
+	const deactivateExtensionCommand = vscode.commands.registerCommand('gopilot-vscode-ext.deactivateExtension', () => {
+		vscode.window.showInformationMessage('Extension deactivated!');
+		activated = false;
+	});
 
-        const document = editor.document;
-        const content = document.getText();
-        const cursorOffset = document.offsetAt(editor.selection.start);
+	const provider: vscode.InlineCompletionItemProvider = {  
+		provideInlineCompletionItems: async (document, position, context, token) => {  
+			if (!activated) {
+				return;
+			}
 
-        try {
-			const response = await axios.post('http://localhost:3000/complete', {
-				content,
-				cursorOffset,
+		  	// Fetching the text before the cursor  
+		  	const textBeforeCursor = document.getText();  
+	  
+			// If there's no text, return no items  
+			if (textBeforeCursor.trim() === "") {  
+				return { items: [] };  
+			}  
+		
+			return new Promise(resolve => {
+				// Set a delay of 1000ms before making a suggestion.
+				timeout = setTimeout(async () => {
+					// Fetching the code completion based on the text in the user's document  
+					let tokens: string[] | undefined;
+
+					try {  
+						const editor = vscode.window.activeTextEditor;
+						if (!editor) {
+							vscode.window.showErrorMessage('No active editor found.');
+							return;
+						}
+				
+						const document = editor.document;
+						const content = document.getText();
+						const cursorOffset = document.offsetAt(editor.selection.start);
+				
+						const response = await axios.post('http://localhost:3000/complete', {
+							content,
+							cursorOffset,
+						});
+		
+						tokens = [response.data.tokens];
+
+						// If there's no response, return no items  
+						if (tokens === null || tokens === undefined) {  
+							resolve({ items: [] });  
+						}  
+
+						// Add the generated code to the inline suggestion list  
+						const items: any[] = [];  
+						for (let i = 0; i < tokens.length; i++) {  
+							items.push({  
+								insertText: tokens[i],  
+								range: new vscode.Range(position.translate(0, tokens[i].length), position),  
+								trackingId: `snippet-${i}`,  
+							});  
+						}
+
+						resolve({ items });  
+					} catch (err) {  
+						vscode.window.showErrorMessage(`Error: ${err}`);
+						resolve({ items: [] });  
+					}  
+				}, 1000);
 			});
-			
-			tokens = response.data.tokens;
+		}
+	};  
 
-			vscode.window.showInformationMessage(`Tokens: ${tokens}`);
-			
-			// Define a decoration with colored text and custom content
-			coloredTextDecoration = vscode.window.createTextEditorDecorationType({
-				color: 'rgba(200, 100, 100, 1)',
-				rangeBehavior: vscode.DecorationRangeBehavior.ClosedClosed,
-				after: {
-					contentText: tokens,
-					color: 'rgba(0, 0, 0, 0.5)',
-					fontStyle: 'italic',
-				},
-			});
-
-			// Apply the decoration at the cursor position
-			const cursorPosition = editor.selection.start;
-			const startPosition = cursorPosition.translate(0, 0);
-			const endPosition = cursorPosition.translate(0, 0); // Arbitrary length for the decoration
-			const decorationRange = new vscode.Range(startPosition, endPosition);
-			editor.setDecorations(coloredTextDecoration, [{ range: decorationRange }]);
-
-			context.subscriptions.push(onDidChangeTextEditorSelection);
-        } catch (error) {
-            vscode.window.showErrorMessage(`Error: ${error}`);
-        }
-    });
-
-	const onDidChangeTextEditorSelection = vscode.window.onDidChangeTextEditorSelection(async (e) => {
-        const editor = e.textEditor;
-
-        if (coloredTextDecoration) {
-            removeSuggestion(editor);
-        }
-    });
-
-    context.subscriptions.push(autoCompleteCommand);
-    context.subscriptions.push(onDidChangeTextEditorSelection);
+	// Registering the provider for all files  
+	vscode.languages.registerInlineCompletionItemProvider({ pattern: "**" }, provider);
 }
 
 export function deactivate() {}
